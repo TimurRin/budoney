ffb3e00a7b1c2bcec064a3dc931ddc6d4fea0b8a
Timur Moziev <code@timurrin.ru>
2022-08-15 02:13:22 +0300

    Better functions in Google Sheets handler module

--- src/main/python/google_sheets_handler.py
+++ src/main/python/google_sheets_handler.py
@@ -1,5 +1,5 @@
 import gspread
-import gspread_dataframe
+from gspread import Spreadsheet
 from oauth2client.service_account import ServiceAccountCredentials
 
 import utils.yaml_manager as yaml_manager
@@ -13,21 +13,67 @@ scope = [
     'https://www.googleapis.com/auth/drive'
 ]
 
-print("Authenticating using  credentials, saved in JSON")
+print("Authenticating using credentials, saved in JSON")
 google_sheets_credentials = ServiceAccountCredentials.from_json_keyfile_name(
     '../../../config/local/google-api.json', scope)
 
-
-def fetch_sheet():
-    print("Initializing the client, and opening the sheet by id")
-    client = gspread.authorize(google_sheets_credentials)
-    return client.open_by_key(google_sheets_config["bookKey"]).get_worksheet_by_id(google_sheets_config["sheetId"])
+gspread_client = None
+data = None
 
 
-def get_data():
-    sheet = fetch_sheet()
-    print("Getting data from the sheet")
-    return gspread_dataframe.get_as_dataframe(sheet, parse_dates=True)
+def check_authorization():
+    global gspread_client
+    if not gspread_client:
+        print("Authorizing Google client")
+        gspread_client = gspread.authorize(google_sheets_credentials)
 
 
-print(get_data())
+def fetch_sheet(name):
+    check_authorization()
+    print("Opening the sheet '" + name + "'")
+    sheet_credentials = google_sheets_config["sheets"][name]
+    return gspread_client.open_by_key(sheet_credentials["bookKey"]).get_worksheet_by_id(sheet_credentials["sheetId"])
+
+
+def fetch_flow_sheet():
+    check_authorization()
+    print("Opening the flow sheet")
+    sheet_credentials = google_sheets_config["sheets"]["flow"]
+    sheet_name = sheet_credentials["sheetPrefix"] + "2022_08"
+    return gspread_client.open_by_key(sheet_credentials["bookKey"]).worksheet(sheet_name)
+
+
+def get_data(sheet: Spreadsheet):
+    print("Getting data from the sheet '" +
+          str(sheet.title) + "' (" + str(sheet.id) + ")")
+    return sheet.get_values()
+
+
+def get_all_sheets():
+    return {
+        "categories": fetch_sheet("categories"),
+        "methods": fetch_sheet("methods"),
+        "venues": fetch_sheet("venues"),
+        "flow": {
+            "2022_08": fetch_flow_sheet()
+        },
+    }
+
+
+def fetch_all_data(sheets):
+    data = {"flow": {}}
+    for sheet in sheets:
+        if sheet == "flow":
+            pass
+            # for flow_sheet in sheets["flow"]:
+            #     data["flow"][flow_sheet] = get_data(sheets["flow"][flow_sheet])
+        else:
+            data[sheet] = get_data(sheets[sheet])
+        pass
+    return data
+
+
+def get_cached_data():
+    if not data:
+        data = fetch_all_data(get_all_sheets())
+    return data

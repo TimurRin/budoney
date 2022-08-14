83343899775ee059868e88e5ff193a87c6a8df07
Timur Moziev <code@timurrin.ru>
2022-08-14 08:24:36 +0300

    Wrapped some code in functions

--- src/main/python/google_sheets_handler.py
+++ src/main/python/google_sheets_handler.py
@@ -4,7 +4,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 import utils.yaml_manager as yaml_manager
 
-print("Loading configs")
+print("Loading google-sheets configs")
 google_sheets_config = yaml_manager.load("config/local/google-sheets")
 
 print("Setting scope to use when authenticating")
@@ -14,13 +14,20 @@ scope = [
 ]
 
 print("Authenticating using  credentials, saved in JSON")
-creds = ServiceAccountCredentials.from_json_keyfile_name('../../../config/local/google-api.json', scope)
+google_sheets_credentials = ServiceAccountCredentials.from_json_keyfile_name(
+    '../../../config/local/google-api.json', scope)
 
-print("Initializing the client, and opening the sheet by id")
-client = gspread.authorize(creds)
-sheet = client.open_by_key(google_sheets_config["bookKey"]).get_worksheet_by_id(google_sheets_config["sheetId"])
 
-print("Getting data from the sheet")
-data = gspread_dataframe.get_as_dataframe(sheet, parse_dates=True)
+def fetch_sheet():
+    print("Initializing the client, and opening the sheet by id")
+    client = gspread.authorize(google_sheets_credentials)
+    return client.open_by_key(google_sheets_config["bookKey"]).get_worksheet_by_id(google_sheets_config["sheetId"])
 
-print(data)
+
+def get_data():
+    sheet = fetch_sheet()
+    print("Getting data from the sheet")
+    return gspread_dataframe.get_as_dataframe(sheet, parse_dates=True)
+
+
+print(get_data())

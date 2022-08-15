ef99f949b943684fcaa4723285dd6e0c70c0d0a2
Timur Moziev <code@timurrin.ru>
2022-08-15 18:00:05 +0300

    Handling Google Sheet sheets and data cache Early keyboard in Telegram bot Better debug print

--- src/main/python/google_sheets_handler.py
+++ src/main/python/google_sheets_handler.py
@@ -4,52 +4,55 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 import utils.yaml_manager as yaml_manager
 
-print("Loading google-sheets configs")
+print_label = "[google_sheets_handler]"
+
+print(print_label, "Loading google-sheets configs")
 google_sheets_config = yaml_manager.load("config/local/google-sheets")
 
-print("Setting scope to use when authenticating")
+print(print_label, "Setting scope to use when authenticating")
 scope = [
     'https://www.googleapis.com/auth/spreadsheets',
     'https://www.googleapis.com/auth/drive'
 ]
 
-print("Authenticating using credentials, saved in JSON")
+print(print_label, "Authenticating using credentials, saved in JSON")
 google_sheets_credentials = ServiceAccountCredentials.from_json_keyfile_name(
     '../../../config/local/google-api.json', scope)
 
 gspread_client = None
+sheets = None
 data = None
 
 
 def check_authorization():
     global gspread_client
     if not gspread_client:
-        print("Authorizing Google client")
+        print(print_label, "Authorizing Google client")
         gspread_client = gspread.authorize(google_sheets_credentials)
 
 
 def fetch_sheet(name):
     check_authorization()
-    print("Opening the sheet '" + name + "'")
+    print(print_label, "Opening the sheet '" + name + "'")
     sheet_credentials = google_sheets_config["sheets"][name]
     return gspread_client.open_by_key(sheet_credentials["bookKey"]).get_worksheet_by_id(sheet_credentials["sheetId"])
 
 
 def fetch_flow_sheet():
     check_authorization()
-    print("Opening the flow sheet")
+    print(print_label, "Opening the flow sheet")
     sheet_credentials = google_sheets_config["sheets"]["flow"]
     sheet_name = sheet_credentials["sheetPrefix"] + "2022_08"
     return gspread_client.open_by_key(sheet_credentials["bookKey"]).worksheet(sheet_name)
 
 
-def get_data(sheet: Spreadsheet):
-    print("Getting data from the sheet '" +
+def fetch_data(sheet: Spreadsheet):
+    print(print_label, "Getting data from the sheet '" +
           str(sheet.title) + "' (" + str(sheet.id) + ")")
     return sheet.get_values()
 
 
-def get_all_sheets():
+def fetch_all_sheets():
     return {
         "categories": fetch_sheet("categories"),
         "methods": fetch_sheet("methods"),
@@ -66,14 +69,28 @@ def fetch_all_data(sheets):
         if sheet == "flow":
             pass
             # for flow_sheet in sheets["flow"]:
-            #     data["flow"][flow_sheet] = get_data(sheets["flow"][flow_sheet])
+            #     data["flow"][flow_sheet] = fetch_data(sheets["flow"][flow_sheet])
         else:
-            data[sheet] = get_data(sheets[sheet])
+            data[sheet] = fetch_data(sheets[sheet])
         pass
     return data
 
 
+def get_cached_sheets():
+    global sheets
+    if not sheets:
+        sheets = fetch_all_sheets()
+    return sheets
+
+
 def get_cached_data():
+    global data
     if not data:
-        data = fetch_all_data(get_all_sheets())
+        data = fetch_all_data(get_cached_sheets())
     return data
+
+
+def invalidate_all():
+    global sheets, data
+    sheets = None
+    data = None
--- src/main/python/telegram_handler.py
+++ src/main/python/telegram_handler.py
@@ -1,6 +1,6 @@
 from random import random
 import telegram
-from telegram import Update, bot
+from telegram import Update, ReplyKeyboardMarkup, bot
 from telegram.ext import Handler
 from telegram.ext.updater import Updater
 from telegram.update import Update
@@ -8,9 +8,53 @@ from telegram.ext.callbackcontext import CallbackContext
 from telegram.ext.commandhandler import CommandHandler
 from telegram.ext.messagehandler import MessageHandler
 from telegram.ext.filters import Filters
-from telegram.ext import Updater, MessageHandler, Filters, CallbackQueryHandler
+from telegram.ext import Updater, Filters, CallbackQueryHandler
+from telegram.ext import ConversationHandler
 
+import google_sheets_handler as gsh
 import utils.yaml_manager as yaml_manager
+
+print_label = "[telegram_handler]"
+
+updater = None
+
+states = {
+    "main": 0,
+    "categories": 1
+}
+
+
+def init():
+    global updater
+    print(print_label, "Starting Telegram updater...")
+    updater = Updater(telegram_config["bot_token"], use_context=True)
+
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', command_start)],
+        states={
+            states["main"]: [MessageHandler(Filters.text & ~Filters.command, command_start)],
+            states["categories"]: [MessageHandler(Filters.text & ~Filters.command, command_categories)],
+        },
+        fallbacks=[CommandHandler('cancel', command_cancel)],
+    )
+
+    updater.dispatcher.add_handler(conv_handler)
+
+    # print(print_label, "Setting Telegram commands...")
+    # add_command('start', command_start)
+    add_command('help', command_help)
+
+    print(print_label, "Setting Telegram handlers...")
+    updater.dispatcher.add_handler(MessageHandler(
+        Filters.command & auth_filter() & conversation_filter(), command_unknown))
+    updater.dispatcher.add_handler(MessageHandler(
+        Filters.text & auth_filter() & conversation_filter(), any_text_input))
+
+    print(print_label, "Starting Telegram polling...")
+    updater.start_polling()
+
+    print(print_label, "Started")
+    updater.idle()
 
 
 def auth_filter():
@@ -27,22 +71,37 @@ def add_command(name, callback):
 
 
 def command_start(update: Update, context: CallbackContext):
-    update.message.reply_text(
-        "Нажми /help, чтобы посмотреть доступные команды")
+    reply_keyboard = [['See categories']]
+    markup_key = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+    update.message.reply_text("What do you want?", reply_markup=markup_key)
+    return states["categories"]
+
+
+def command_categories(update: Update, context: CallbackContext):
+    data = gsh.get_cached_data()
+    reply_keyboard = [[data["categories"][0][0], data["categories"][1][0], data["categories"][2][0]], [
+        data["categories"][3][0], data["categories"][4][0], data["categories"][5][0]]]
+    markup_key = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+    update.message.reply_text("Select your category", reply_markup=markup_key)
+    return states["main"]
 
 
 def command_help(update: Update, context: CallbackContext):
     update.message.reply_text("Нет никакой помощи. Беги.")
 
 
+def command_cancel(update: Update, context: CallbackContext):
+    update.message.reply_text("Нет никакой отмены. Беги.")
+
+
 def any_text_input(update: Update, context: CallbackContext):
-    print(update.message)
+    print(print_label, update.message)
     update.message.reply_text(
         "ты хоть сам понял что ввёл?")
 
 
 def command_unknown(update: Update, context: CallbackContext):
-    print(update.message)
+    print(print_label, update.message)
     update.message.reply_text(
         "ъуъ сам ты '%s'! get some /help" % update.message.text)
 
@@ -51,24 +110,7 @@ def send_info_to_chats():
     pass
 
 
-print("Loading Telegram configs...")
+print(print_label, "Loading Telegram configs...")
 telegram_config = yaml_manager.load("config/local/telegram")
 
-print("Starting Telegram updater...")
-updater = Updater(telegram_config["bot_token"], use_context=True)
-
-print("Setting Telegram commands...")
-add_command('start', command_start)
-add_command('help', command_help)
-
-print("Setting Telegram handlers...")
-updater.dispatcher.add_handler(MessageHandler(
-    Filters.command & auth_filter() & conversation_filter(), command_unknown))
-updater.dispatcher.add_handler(MessageHandler(
-    Filters.text & auth_filter() & conversation_filter(), any_text_input))
-
-print("Starting Telegram polling...")
-updater.start_polling()
-
-print("Started")
-updater.idle()
+init()

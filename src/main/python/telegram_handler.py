52869f2b9935a832e7b263ff581e8b0fc21f6bf2
Timur Moziev <code@timurrin.ru>
2022-08-18 23:18:26 +0300

    New states and better main keyboard Code refactoring Added transliterate module

--- src/main/python/telegram_handler.py
+++ src/main/python/telegram_handler.py
@@ -1,18 +1,20 @@
-from random import random
 import json
+from random import random
+
 import telegram
-from telegram import Update, ReplyKeyboardMarkup, InlineKeyboardMarkup, ReplyKeyboardRemove, bot
-from telegram.ext import Handler
-from telegram.ext.updater import Updater
-from telegram.update import Update
+from telegram import (InlineKeyboardMarkup, Message, ReplyKeyboardMarkup,
+                      ReplyKeyboardRemove, Update, bot)
+from telegram.ext import (CallbackQueryHandler, ConversationHandler, Filters,
+                          Handler, Updater)
 from telegram.ext.callbackcontext import CallbackContext
 from telegram.ext.commandhandler import CommandHandler
-from telegram.ext.messagehandler import MessageHandler
 from telegram.ext.filters import Filters
-from telegram.ext import Updater, Filters, CallbackQueryHandler
-from telegram.ext import ConversationHandler
+from telegram.ext.messagehandler import MessageHandler
+from telegram.ext.updater import Updater
+from telegram.update import Update
 
 import google_sheets_handler as gsh
+import utils.transliterate as transliterate
 import utils.yaml_manager as yaml_manager
 
 print_label = "[telegram_handler]"
@@ -21,10 +23,25 @@ updater = None
 
 states = {
     "main": 0,
-    "categories": 1,
-    "flow_sum": 2,
-    "flow_method": 3,
-    "flow_venue": 4,
+    "wip": 1,
+
+    "flows": 100,
+    "flow_add_sum": 105,
+    "flow_add_method": 106,
+    "flow_add_venue": 107,
+
+    "venues": 200,
+    "venue_add_name": 205,
+    "venue_add_category": 210,
+
+    "methods": 300,
+    "method_add_name": 305,
+    "method_add_is_credit": 310,
+    "method_add_owner": 315,
+
+    "categories": 400,
+
+    "currencies": 500,
 }
 
 main_entry_text = "Shall we begin?"
@@ -40,11 +57,21 @@ def init():
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('start', command_start)],
         states={
-            states["main"]: [CallbackQueryHandler(command_main_callback)],
-            states["categories"]: [CallbackQueryHandler(command_categories_callback)],
-            states["flow_sum"]: [default_text_parse_handler()],
-            states["flow_method"]: [CallbackQueryHandler(command_methods_callback)],
-            states["flow_venue"]: [CallbackQueryHandler(command_venues_callback)],
+            states["main"]: [CallbackQueryHandler(handle_main)],
+            states["wip"]: [CallbackQueryHandler(handle_wip)],
+            states["flows"]: [CallbackQueryHandler(handle_flows)],
+            states["flow_add_sum"]: [MessageHandler(text_filters(), handle_flow_add_sum)],
+            states["flow_add_method"]: [CallbackQueryHandler(handle_flow_add_method)],
+            states["flow_add_venue"]: [CallbackQueryHandler(handle_flow_add_venue)],
+            states["venues"]: [CallbackQueryHandler(handle_venues)],
+            states["venue_add_name"]: [MessageHandler(text_filters(), handle_venue_add_name)],
+            states["venue_add_category"]: [CallbackQueryHandler(handle_venue_add_category)],
+            states["methods"]: [CallbackQueryHandler(handle_methods)],
+            states["method_add_name"]: [MessageHandler(text_filters(), handle_method_add_name)],
+            states["method_add_is_credit"]: [CallbackQueryHandler(handle_method_add_is_credit)],
+            states["method_add_owner"]: [CallbackQueryHandler(handle_method_add_owner)],
+            states["categories"]: [CallbackQueryHandler(handle_categories)],
+            states["currencies"]: [CallbackQueryHandler(handle_currencies)],
         },
         fallbacks=[CommandHandler('start', command_start)],
     )
@@ -72,9 +99,11 @@ def init():
     updater.idle()
 
 
-def default_text_parse_handler():
-    return MessageHandler(
-        Filters.text & auth_filter() & conversation_filter(), default_text_parse)
+# local utils
+
+
+def text_filters():
+    return Filters.text & auth_filter() & conversation_filter()
 
 
 def send_message_to_authorized(message):
@@ -106,46 +135,55 @@ def add_command(name, callback):
         CommandHandler(name, callback, auth_filter() & conversation_filter()))
 
 
-def default_keyboard():
+def check_common_data():
+    pass
+
+
+# keyboards
+
+
+def keyboard_template_default():
     return [[]]
-    # return [[telegram.InlineKeyboardButton(text="🔙 Back", callback_data="_BACK")]]
 
 
-def command_start(update: Update, context: CallbackContext):
-    update.message.reply_text(
-        "🍏 Fresh start! " + main_entry_text, reply_markup=command_main_keyboard())
-    return states["main"]
+def keyboard_template_with_back():
+    return [[telegram.InlineKeyboardButton(text="🔙 Back", callback_data="_BACK")]]
 
 
-def command_main(update: Update, context: CallbackContext):
-    update.callback_query.message.edit_text(
-        main_entry_text, reply_markup=command_main_keyboard())
-    return states["main"]
-
-
-def command_main_keyboard():
-    reply_keyboard = [[telegram.InlineKeyboardButton(
-        text="Add flow entry", callback_data="flow_sum"), telegram.InlineKeyboardButton(
-        text="See categories", callback_data="categories")]]
+def keyboard_main():
+    reply_keyboard = [
+        [
+            telegram.InlineKeyboardButton(
+                text="👛 Flows", callback_data="flows"),
+            telegram.InlineKeyboardButton(
+                text="💸 Add expense", callback_data="flow_add_sum"),
+            telegram.InlineKeyboardButton(
+                text="💰 Add income", callback_data="flow_add_sum"),
+        ],
+        [
+            telegram.InlineKeyboardButton(
+                text="🏪 Venues", callback_data="venues"),
+        ],
+        [
+            telegram.InlineKeyboardButton(
+                text="💳 Methods", callback_data="methods"),
+        ],
+        [
+            telegram.InlineKeyboardButton(
+                text="🏷 Categories", callback_data="categories"),
+        ],
+        [
+            telegram.InlineKeyboardButton(
+                text="💱 Currencies", callback_data="currencies"),
+        ]
+    ]
     return InlineKeyboardMarkup(reply_keyboard)
 
 
-def command_main_callback(update: Update, context: CallbackContext):
-    if update.callback_query.data == "categories":
-        update.callback_query.message.edit_text("Select your category",
-                                                reply_markup=command_categories_keyboard())
-        return states["categories"]
-    elif update.callback_query.data == "flow_sum":
-        update.callback_query.message.edit_text("Enter sum")
-        return states["flow_sum"]
-    else:
-        return states["main"]
-
-
-def command_categories_keyboard():
+def keyboard_categories():
     data = gsh.get_cached_data()
 
-    reply_keyboard = default_keyboard()
+    reply_keyboard = keyboard_template_with_back()
 
     current_row = 0
 
@@ -159,10 +197,10 @@ def command_categories_keyboard():
     return InlineKeyboardMarkup(reply_keyboard)
 
 
-def command_methods_keyboard():
+def keyboard_methods():
     data = gsh.get_cached_data()
 
-    reply_keyboard = default_keyboard()
+    reply_keyboard = keyboard_template_with_back()
 
     current_row = 0
 
@@ -176,10 +214,10 @@ def command_methods_keyboard():
     return InlineKeyboardMarkup(reply_keyboard)
 
 
-def command_venues_keyboard():
+def keyboard_venues():
     data = gsh.get_cached_data()
 
-    reply_keyboard = default_keyboard()
+    reply_keyboard = keyboard_template_with_back()
 
     current_row = 0
 
@@ -193,58 +231,149 @@ def command_venues_keyboard():
     return InlineKeyboardMarkup(reply_keyboard)
 
 
-def command_categories_callback(update: Update, context: CallbackContext):
-    # print("command_categories_callback", update)
-
-    if (update.callback_query.data != "_BACK"):
-        data = gsh.get_cached_data()
-        send_info_message(update.callback_query.from_user.first_name + " has selected " + data["categories"]["dict"][update.callback_query.data]["emoji"] + " "
-                          + data["categories"]["dict"][update.callback_query.data]["name"] + " and everyone should be aware of it")
-
-    return command_main(update, context)
+def keyboard_with_back():
+    return InlineKeyboardMarkup(keyboard_template_with_back())
 
 
-def command_methods_callback(update: Update, context: CallbackContext):
-    # print("command_categories_callback", update)
+# commands
 
-    if (update.callback_query.data != "_BACK"):
-        data = gsh.get_cached_data()
-        send_info_message(update.callback_query.from_user.first_name + " has selected " +
-                          data["methods"]["dict"][update.callback_query.data]["name"] + " and everyone should be aware of it")
-
-    update.callback_query.message.reply_text("Where?",
-                                             reply_markup=command_venues_keyboard())
-    return states["flow_venue"]
-
-
-def command_venues_callback(update: Update, context: CallbackContext):
-    # print("command_categories_callback", update)
-
-    if (update.callback_query.data != "_BACK"):
-        data = gsh.get_cached_data()
-        send_info_message(update.callback_query.from_user.first_name + " has selected " +
-                          data["venues"]["dict"][update.callback_query.data]["name"] + " and everyone should be aware of it")
-
-    return command_main(update, context)
+def command_start(update: Update, context: CallbackContext):
+    update.message.reply_text(
+        "🍏 Fresh start! " + main_entry_text, reply_markup=keyboard_main())
+    return states["main"]
 
 
 def command_help(update: Update, context: CallbackContext):
     update.message.reply_text("You'll get no help here. Run.")
 
 
-def default_text_parse(update: Update, context: CallbackContext):
-    print("command_main_text_parse", update.message.text)
-    # context.bot.delete_message(update.message.chat_id, str(update.message.message_id))
+# states
 
+def state_main(message: Message):
+    message.edit_text(
+        main_entry_text, reply_markup=keyboard_main())
+    return states["main"]
+
+
+# handlers
+
+def handle_main(update: Update, context: CallbackContext):
+    if update.callback_query.data == "main":
+        return states["main"]
+    elif update.callback_query.data == "flows":
+        update.callback_query.message.edit_text(
+            "List of flows", reply_markup=keyboard_with_back())
+        return states["flows"]
+    elif update.callback_query.data == "flow_add_sum":
+        update.callback_query.message.edit_text("Enter sum")
+        return states["flow_add_sum"]
+    elif update.callback_query.data == "venues":
+        update.callback_query.message.edit_text("List of venues",
+                                                reply_markup=keyboard_venues())
+        return states["venues"]
+    elif update.callback_query.data == "methods":
+        update.callback_query.message.edit_text("List of methods",
+                                                reply_markup=keyboard_methods())
+        return states["methods"]
+    elif update.callback_query.data == "categories":
+        update.callback_query.message.edit_text("List of categories",
+                                                reply_markup=keyboard_categories())
+        return states["categories"]
+    else:
+        update.callback_query.message.edit_text("🏗️ This section is not ready",
+                                                reply_markup=keyboard_with_back())
+        return states["wip"]
+
+
+def handle_flows(update: Update, context: CallbackContext):
+    return state_main(update.callback_query.message)
+
+
+def handle_flow_add_sum(update: Update, context: CallbackContext):
     try:
         sum = float(update.message.text)
 
-        update.message.reply_text("How?",
-                                  reply_markup=command_methods_keyboard())
+        update.message.reply_text(str(sum) + " roubles. How?",
+                                  reply_markup=keyboard_methods())
 
-        return states["flow_method"]
+        return states["flow_add_method"]
     except ValueError:
         return states["main"]
+
+    return state_main(update.callback_query.message)
+
+
+def handle_flow_add_method(update: Update, context: CallbackContext):
+    update.callback_query.message.edit_text(str(update.callback_query.data) + ". Where?",
+                                            reply_markup=keyboard_venues())
+
+    return states["flow_add_venue"]
+
+
+def handle_flow_add_venue(update: Update, context: CallbackContext):
+    return state_main(update.callback_query.message)
+
+
+def handle_venues(update: Update, context: CallbackContext):
+    if (update.callback_query.data != "_BACK"):
+        data = gsh.get_cached_data()
+        send_info_message(update.callback_query.from_user.first_name + " has selected " +
+                          data["venues"]["dict"][update.callback_query.data]["name"] + " and everyone should be aware of it")
+
+    return state_main(update.callback_query.message)
+
+
+def handle_venue_add_name(update: Update, context: CallbackContext):
+    return state_main(update.callback_query.message)
+
+
+def handle_venue_add_category(update: Update, context: CallbackContext):
+    return state_main(update.callback_query.message)
+
+
+def handle_methods(update: Update, context: CallbackContext):
+    if (update.callback_query.data != "_BACK"):
+        data = gsh.get_cached_data()
+        send_info_message(update.callback_query.from_user.first_name + " has selected " +
+                          data["methods"]["dict"][update.callback_query.data]["name"] + " and everyone should be aware of it")
+
+    return state_main(update.callback_query.message)
+
+
+def handle_method_add_name(update: Update, context: CallbackContext):
+    return state_main(update.callback_query.message)
+
+
+def handle_method_add_is_credit(update: Update, context: CallbackContext):
+    return state_main(update.callback_query.message)
+
+
+def handle_method_add_owner(update: Update, context: CallbackContext):
+    return state_main(update.callback_query.message)
+
+
+def handle_categories(update: Update, context: CallbackContext):
+    if (update.callback_query.data != "_BACK"):
+        data = gsh.get_cached_data()
+        send_info_message(update.callback_query.from_user.first_name + " has selected " + data["categories"]["dict"][update.callback_query.data]["emoji"] + " "
+                          + data["categories"]["dict"][update.callback_query.data]["name"] + " and everyone should be aware of it")
+
+    return state_main(update.callback_query.message)
+
+
+def handle_currencies(update: Update, context: CallbackContext):
+    return state_main(update.callback_query.message)
+
+
+def handle_wip(update: Update, context: CallbackContext):
+    update.callback_query.message.edit_text(
+        main_entry_text, reply_markup=keyboard_main())
+    return states["main"]
+
+
+# context.bot.delete_message(update.message.chat_id, str(update.message.message_id))
+
+# fallbacks
 
 
 def fallback(update: Update, context: CallbackContext):
@@ -255,10 +384,6 @@ def fallback(update: Update, context: CallbackContext):
 def fallback_callback_query_handler(update: Update, context: CallbackContext):
     # print(print_label, "fallback_callback_query_handler", update)
     update.callback_query.message.reply_text(error_text)
-
-
-def send_info_to_chats():
-    pass
 
 
 print(print_label, "Loading Telegram configs...")

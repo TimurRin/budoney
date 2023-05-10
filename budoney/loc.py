17a874157fef1d2b06f3a4f35a512baaa7e71bf0
Timur Moziev <code@timurrin.ru>
2023-05-10 13:35:12 +0300

    move lang values from data to config

--- budoney/loc.py
+++ budoney/loc.py
@@ -2,4 +2,4 @@ import utils.yaml_manager as yaml_manager
 import configs
 
 print("[budoney]", "Loading localization...")
-localization = yaml_manager.load(f"data/loc/{configs.general['localization']}")
+localization = yaml_manager.load(f"config/static/loc/{configs.general['localization']}")

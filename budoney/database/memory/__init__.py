976c0a520abfe78b9fb62b9b5526621a41c7e551
Timur Moziev <code@timurrin.ru>
2023-05-10 16:47:36 +0300

    fix memory db

--- budoney/database/memory/__init__.py
+++ budoney/database/memory/__init__.py
@@ -4,42 +4,41 @@ import configs
 
 
 class MemoryDatabase(Database):
-    data: dict[dict] = {}
-    record_ids = 0
+    db = {"data": {}, "internal": {"record_ids": 0}}
 
     def __init__(self):
         saved = yaml_manager.load("data/memory")
         if saved:
-            self.data = saved
+            self.db = saved
         else:
-            self.data = yaml_manager.load("data/memory-original")
+            self.db["data"] = yaml_manager.load("data/memory-original")
 
     def get_record(self, table, record_id):
         self._check_table(table)
-        return record_id in self.data[table] and self.data[table] or None
+        return record_id in self.db["data"][table] and self.db["data"][table] or None
 
     def get_records(self, table):
         self._check_table(table)
-        return list(self.data[table].values())
+        return list(self.db["data"][table].values())
 
     def replace_data(self, table, data):
         self._check_table(table)
         data = self._check_data(dict(data))
-        self.data[table][data["id"]] = data
-        yaml_manager.save("data/memory", self.data)
+        self.db["data"][table][data["id"]] = data
+        yaml_manager.save("data/memory", self.db)
 
     def append_data(self, table, data):
         self._check_table(table)
         data = self._check_data(dict(data))
-        self.data[table][data["id"]] = data
-        yaml_manager.save("data/memory", self.data)
+        self.db["data"][table][data["id"]] = data
+        yaml_manager.save("data/memory", self.db)
 
     def _check_table(self, table):
-        if table not in self.data:
-            self.data[table] = {}
+        if table not in self.db["data"]:
+            self.db["data"][table] = {}
 
     def _check_data(self, data):
         if "id" not in data:
-            data["id"] = f"ID_{self.record_ids}"
-            self.record_ids = self.record_ids + 1
+            data["id"] = f"ID_{self.db['internal']['record_ids']}"
+            self.db["internal"]["record_ids"] = self.db["internal"]["record_ids"] + 1
         return data

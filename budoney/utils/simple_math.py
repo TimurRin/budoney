e5bf19e890eaf11fe5630a7cb41f2da69af5a336
Timur Moziev <code@timurrin.ru>
2023-08-26 01:42:55 +0300

    fix calculate

--- budoney/utils/simple_math.py
+++ budoney/utils/simple_math.py
@@ -2,7 +2,10 @@ def calculate(expression: str) -> float:
     expression = expression.replace(" ", "")
     splitted = expression.split("+")
     sum = 0
+    print(splitted)
     for part in splitted:
-        if part.isnumeric():
+        try:
             sum += float(part)
+        except:
+            pass
     return sum
